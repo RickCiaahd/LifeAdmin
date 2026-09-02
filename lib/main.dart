aa14d53767d8159f34a267cdb0003c6c3c26312e
Riccardo Forte <59015717+RickCiaahd@users.noreply.github.com>
2026-09-02 16:33:00 +0200

    Initialize local app state before launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'src/data/local_store.dart';
+import 'src/state/app_controller.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const LifeAdminApp());
+  final controller = AppController(LocalStore());
+  await controller.load();
+  runApp(LifeAdminApp(controller: controller));
 }
