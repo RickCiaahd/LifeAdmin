e9b341513bf94c07a2ef469c7c14bac935b09f83
Riccardo Forte <59015717+RickCiaahd@users.noreply.github.com>
2026-09-02 16:31:33 +0200

    Add serialization for life objects

--- lib/src/domain/life_object.dart
+++ lib/src/domain/life_object.dart
@@ -12,4 +12,23 @@ class LifeObject {
   final String name;
   final LifeObjectType type;
   final String? details;
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'type': type.name,
+        'details': details,
+      };
+
+  factory LifeObject.fromJson(Map<String, dynamic> json) {
+    return LifeObject(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      type: LifeObjectType.values.firstWhere(
+        (value) => value.name == json['type'],
+        orElse: () => LifeObjectType.other,
+      ),
+      details: json['details'] as String?,
+    );
+  }
 }
