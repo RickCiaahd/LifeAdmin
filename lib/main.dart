56a9079409338a9cda2c6a62e9f443f71acfc5b9
Riccardo Forte <59015717+RickCiaahd@users.noreply.github.com>
2026-09-02 16:38:26 +0200

    Initialize notification service at startup

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 import 'src/app.dart';
 import 'src/data/local_store.dart';
+import 'src/notifications/notification_service.dart';
 import 'src/state/app_controller.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final controller = AppController(LocalStore());
+
+  final notifications = NotificationService();
+  await notifications.initialize();
+
+  final controller = AppController(LocalStore(), notifications);
   await controller.load();
+
   runApp(LifeAdminApp(controller: controller));
 }
